ecdad147a8fcc4602cbd5a4c3ec73c6aa9a8c26f
bobetbat <b.bobetayev.97@gmail.com>
2022-11-12 17:49:43 +0100

    fix: fix style

--- packages/site/src/components/Form.tsx
+++ packages/site/src/components/Form.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Address, Bee, BeeDebug, PostageBatch } from '@ethersphere/bee-js';
-import { IconButton, TextareaAutosize } from '@mui/material';
+import { Button } from './Buttons'
+import { IconButton, Stack, TextareaAutosize, Typography } from '@mui/material';
 import ThumbDownRoundedIcon from '@mui/icons-material/ThumbDownRounded';
 import ThumbUpRoundedIcon from '@mui/icons-material/ThumbUpRounded';
+import styled from 'styled-components';
 
 const beeUrl = "http://localhost:1633"
 const beeDebugUrl = "http://localhost:1635"
@@ -10,6 +12,27 @@ const POSTAGE_STAMPS_AMOUNT = BigInt(10000)
 const POSTAGE_STAMPS_DEPTH = 20
 const bee = new Bee(beeUrl);
 const beeDebug = new BeeDebug(beeDebugUrl);
+
+const CardWrapper = styled.div<{ fullWidth?: boolean; disabled: boolean }>`
+  display: flex;
+  flex-direction: column;
+  width: ${({ fullWidth }) => (fullWidth ? '100%' : '250px')};
+  background-color: ${({ theme }) => theme.colors.card.default};
+  margin-top: 2.4rem;
+  margin-bottom: 2.4rem;
+  padding: 2.4rem;
+  border: 1px solid ${({ theme }) => theme.colors.border.default};
+  border-radius: ${({ theme }) => theme.radii.default};
+  box-shadow: ${({ theme }) => theme.shadows.default};
+  filter: opacity(${({ disabled }) => (disabled ? '.4' : '1')});
+  align-self: stretch;
+  ${({ theme }) => theme.mediaQueries.small} {
+  width: 100%;
+    margin-top: 1.2rem;
+    margin-bottom: 1.2rem;
+    padding: 1.6rem;
+  }
+`;
 
 export const Form = () => {
   // const [file, setFile] = useState<File | null>(null)
@@ -53,17 +76,24 @@ export const Form = () => {
   const handleSelectPostageStamp = (event: React.ChangeEvent<HTMLInputElement>) => { setSelectedPostageStamp(event.target.value as Address) }
 
   // should upload json file to storage
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSubmit = async () => {
+    // event.preventDefault();
 
     if (!selectedPostageStamp) return
 
-    if (file) {
+    let data = {
+      like: like,
+      contract: null,
+      message: '',// signed????
+      sign: null // ?????
+    }
+
+    if (data.contract !== null && data.sign !== null) {
       try {
         setUploading(true)
         setLink(null)
 
-        const { reference } = await bee.uploadFile(selectedPostageStamp, file)
+        const { reference } = await bee.uploadFile(selectedPostageStamp, JSON.stringify(data))
         setLink(`${beeUrl}/bzz/${reference}`)
       } catch (e) {
         setError(e)
@@ -74,16 +104,17 @@ export const Form = () => {
     }
   }
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const f = event.target && event.target.files && event.target.files[0]
 
+  const onChange = (event: React..ChangeEventHandler<HTMLTextAreaElement>) => {
+    const f = event.target && event.target.value && event.target.value
+    setComment(f)
     // setFile(f)
     setError(null)
     setLink(null)
   }
 
   return (
-    <div>
+    <CardWrapper fullWidth disabled={false}>
       {/* <h1>Postage stamps</h1>
       <code>
         <div style={{
@@ -110,37 +141,44 @@ export const Form = () => {
           </div>)}
         <hr />
       </code> */}
-      <button onClick={createPostageStamp}>Create new postage stamp</button>
+      {/* <button onClick={createPostageStamp}>Create new postage stamp</button>
       <code>
         {loadingStamps && <span>Loading postage stamps...</span>}
         {creatingStamp && <span>Creating new postage stamp...</span>}
         {stampError && <span>{stampError.message}</span>}
-      </code>
+      </code> */}
 
-      <h1>Upload file to Swarm</h1>
+      <Typography textAlign='center' variant='h3'>Report Contract</Typography>
       <form onSubmit={handleSubmit}>
-        <IconButton color={like ? "primary" : "inherit"} aria-label="upload picture" component="label" onClick={() => setLike(true)}>
-          <ThumbUpRoundedIcon fontSize="large" />
-        </IconButton>
-        <IconButton color={!like ? "primary" : "inherit"} aria-label="upload picture" component="label" onClick={() => setLike(false)}>
-          <ThumbDownRoundedIcon fontSize="large" />
-        </IconButton>
-        <TextareaAutosize
-          maxRows={4}
-          aria-label="maximum height"
-          placeholder="...comment"
-          style={{ width: 200 }}
-        />
-        {/* <input type="file" name="file" onChange={onFileChange} /> */}
-        <input type="submit" disabled={!comment || selectedPostageStamp === null} />
+        <Stack direction='column' spacing={2}>
+          <Stack direction='row' justifyContent="space-evenly">
+            <IconButton color={like ? "primary" : "inherit"} aria-label="upload picture" component="label" onClick={() => setLike(true)}>
+              <ThumbUpRoundedIcon fontSize="large" />
+            </IconButton>
+            <IconButton color={!like ? "primary" : "inherit"} aria-label="upload picture" component="label" onClick={() => setLike(false)}>
+              <ThumbDownRoundedIcon fontSize="large" />
+            </IconButton>
+          </Stack>
+
+          <TextareaAutosize
+            maxRows={4}
+            aria-label="maximum height"
+            placeholder="...comment"
+            style={{ width: 'stretch-content', height: '100px' }}
+            onChange={onChange}
+          />
+
+          <Button style={{ width: '100%' }} onClick={() => handleSubmit()}>
+            Submit
+          </Button>
+        </Stack>
       </form>
-      <br />
-      <code>
+      {/* <code>
         {selectedPostageStamp === null && <span>Please select a postage stamp to use for the file upload</span>}
         {uploading && <span>Uploading...</span>}
         {link && <a href={link} target="blank" >{link}</a>}
         {error && <span>{error.message}</span>}
-      </code>
-    </div>
+      </code> */}
+    </CardWrapper>
   );
 }
